ef505ba5e9fd64553124f128f696d8ec2512b8da
Kirk Bamba <kirkbamba@gmail.com>
2025-01-29 22:36:21 +0800

    added unit testing

---
+++ src/test/java/clinic/programming/training/ApplicationTest.java
@@ -0,0 +1,47 @@
+package clinic.programming.training;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ApplicationTest {
+    private Application app;
+    
+    @BeforeEach
+    public void setup() {
+        app = new Application();
+    }
+    
+    
+    @Test
+    public void testWordCountString() {
+        int count = app.countWords("this is a test");
+        assertTrue(count == 4);
+    }
+
+    @Test
+    public void testWordCountEmpty() {
+        int count = app.countWords("");
+        assertTrue(count == 0);
+    }
+
+    @Test
+    public void testWordCountNull() {
+        int count = app.countWords(null);
+        assertTrue(count == 0);
+    }
+
+    @Test
+    @Disabled
+    public void testWordCountMore() {
+        int count = app.countWords("this will be skipped");
+        assertTrue(count == 3);
+    }
+
+    @Test
+    public void testWordCountSingle() {
+        int count = app.countWords("test");
+        assertTrue(count == 1);
+    }
+}
